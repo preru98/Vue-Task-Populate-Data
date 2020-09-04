c9b92f9a18b48faf8e8d9bcefc916c686c519c47
Prerna <prerna1998.ps@gmail.com>
2020-09-04 23:51:43 +0530

    Table Styling | API Data

--- src/components/PopulateData.vue
+++ src/components/PopulateData.vue
@@ -1,45 +1,60 @@
 <template>
   <div class="populate">
     <h1>Populate</h1>
-    <table v-if="repoData.length" id="query-table" border="1 px">
+    <table v-if="repoData.length" id="query-table">
         <tr>
-            <td>Email</td>
-            <td>Query</td>
+            <th>Email</th>
+            <th>Query</th>
+            <th>Query Date</th>
         </tr>
-        <tr v-for="record in repoData" :key="record.email">
+        <tr v-for="record in repoData" :key="record.query"> <!-- Ideally it should be email -->
             <td>{{ record.email }}</td>
             <td>{{ record.query }}</td>
+            <td>{{ record.createdAt }}</td>
+            
         </tr>
     </table>
     <p v-else>Not available</p>
-
   </div>
 </template>
 
 <script>
 import * as axios from 'axios'
 export default {
-  name: 'PopulateData',
-  data: function(){
-    return {
-        repoData :[
-            {
-                "email": "admin@argames1.com",
-                "query": "testing1"
+    name : 'PopulateData',
+    data : () => {
+        return {
+            repoData:[]
+        }
+    },
+    mounted : function(){
+        axios({
+            method: 'post',
+            url: 'http://66.175.217.67:3020/argames/getQuery',
+            data: {
+                "checkFilter":""
             },
-            {
-                "email": "admin@argames2.com",
-                "query": "testing2",
+            headers: {
+                Authorization: `Bearer 5afe34f322478bc5352a3d988ed4ccb18f7a409160339038c83c772c8a6a7387eb3caf90d064861965eec7eb210286ba` 
             }
-        ]
+        })
+        .then( (response) => {   
+            console.log(response.data);
+            console.log(this);
+            this.repoData = response.data.result;
+            console.log(this.repoData);
+        })
+        .catch( (error) => {
+            console.log(error);
+        })
     }
-  }
+    
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -52,5 +67,29 @@ li {
 }
 a {
   color: #42b983;
+} */
+table {
+    width :100%;
+    border-collapse: collapse;
 }
+th{
+    color:black;
+    font-weight: 700;
+    text-align: center;
+    border : 1px solid grey;
+    padding : 10px;
+}
+table, td {
+    border : 1px solid grey;
+    text-align : left;
+    padding : 10px;
+}
+body{
+    color:black;
+}
+tr:nth-child(odd) {
+    background-color: #F2F2F8;
+}
+
+
 </style>
