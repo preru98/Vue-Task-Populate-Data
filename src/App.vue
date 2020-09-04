7912a08de1ea2aec81969b26ff28f2eb6552af49
Prerna <prerna1998.ps@gmail.com>
2020-09-04 21:31:27 +0530

    Setting up Vue App

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <PopulateData/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PopulateData from './components/PopulateData'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PopulateData
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
